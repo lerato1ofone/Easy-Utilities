3825faeca0b1d0770a545c25d306b46b2231bbfa
Lerato Letsepe <letsepe100@gmail.com>
2021-02-02 23:14:12 +0200

    Added base layout and text for the home screen. v1.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,8 +8,100 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 0.0)),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 25.0),
+              child: Text(
+                'Easily Track Your Utility Spend.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.purple[10],
+                  fontSize: 24.0,
+                  fontFamily: 'Nunito',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.0,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 120.0,
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  MaterialButton(
+                    onPressed: () {},
+                    color: Colors.blue,
+                    textColor: Colors.deepOrange[50],
+                    child: Icon(
+                      Icons.power,
+                      size: 40,
+                    ),
+                    padding: EdgeInsets.all(16),
+                    shape: CircleBorder(),
+                  ),
+                  MaterialButton(
+                    onPressed: () {},
+                    color: Colors.blue,
+                    textColor: Colors.deepOrange[50],
+                    child: Icon(
+                      Icons.water_damage,
+                      size: 40,
+                    ),
+                    padding: EdgeInsets.all(16),
+                    shape: CircleBorder(),
+                  ),
+                ]),
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              'R 1200.00',
+              style: TextStyle(
+                fontSize: 30.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Overall Spend',
+              style: TextStyle(fontSize: 15.0, letterSpacing: 1.0),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color.fromRGBO(192, 63, 138, 50),
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.addchart),
+              label: 'Add Utility',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.data_usage_outlined,
+              ),
+              label: 'Statistics',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance),
+              label: 'Account',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
