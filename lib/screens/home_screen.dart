de4fb8db788bc21e4c281636819c4464a7a6e1a1
Lerato Letsepe <letsepe100@gmail.com>
2021-02-10 00:33:10 +0200

    Swapped Home and Landing screen names, added navbar color, and cleared imports.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,72 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:easy_utilities/screens/account_screen.dart';
-import 'package:easy_utilities/screens/landing_screen.dart';
-import 'package:easy_utilities/screens/add_bill.dart';
-import 'package:easy_utilities/screens/stats_screen.dart';
+import 'package:easy_utilities/constants.dart' as Constants;
 
 class HomeScreen extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _LandingScreenState createState() => _LandingScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-
-  PageController _pageController = PageController();
-
-  List<Widget> _screens = [
-    LandingScreen(),
-    BillsScreen(bill: null),
-    StatsScreen(),
-    AccountScreen(),
-  ];
-
-  void _onItemTapped(int selectIndex) {
-    _pageController.animateToPage(selectIndex,
-        duration: Duration(milliseconds: 500), curve: Curves.easeIn);
-  }
-
-  void _onPageChanged(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class _LandingScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: PageView(
-          controller: _pageController,
-          children: _screens,
-          onPageChanged: _onPageChanged,
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          currentIndex: _selectedIndex,
-          selectedLabelStyle: TextStyle(color: Colors.white),
-          backgroundColor: Colors.blue,
-          selectedItemColor: Colors.white,
-          onTap: _onItemTapped,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.only(top: 50.0)),
+          Container(
+            child: Text(
+              'Easily Track Your Utility Spend.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.purple[10],
+                fontSize: 24.0,
+                fontFamily: 'Nunito',
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.0,
+              ),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.money),
-              label: 'Add Utility',
+          ),
+          SizedBox(
+            height: 100.0,
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: [
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/bills',
+                            arguments: Constants.ELECTRICITY);
+                      },
+                      color: Colors.blue,
+                      textColor: Colors.deepOrange[50],
+                      child: Icon(
+                        Icons.power,
+                        size: 40,
+                      ),
+                      padding: EdgeInsets.all(16),
+                      shape: CircleBorder(),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 20.0)),
+                    Text('Add Electricity bill'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushNamed('/bills', arguments: Constants.WATER);
+                      },
+                      color: Colors.blue,
+                      textColor: Colors.deepOrange[50],
+                      child: Icon(
+                        Icons.water_damage,
+                        size: 40,
+                      ),
+                      padding: EdgeInsets.all(16),
+                      shape: CircleBorder(),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 20.0)),
+                    Text('Add Water bill'),
+                  ],
+                ),
+              ]),
+          SizedBox(
+            height: 80,
+          ),
+          Text(
+            'R 1200.00',
+            style: TextStyle(
+              fontSize: 30.0,
+              letterSpacing: 2.0,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.data_usage_outlined),
-              label: 'Statistics',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Account',
-            ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'OVERALL SPEND',
+            style: TextStyle(fontSize: 15.0, letterSpacing: 1.0),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,96 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:easy_utilities/constants.dart' as Constants;
+import 'package:easy_utilities/screens/account_screen.dart';
+import 'package:easy_utilities/screens/home_screen.dart';
+import 'package:easy_utilities/screens/bills_screen.dart';
+import 'package:easy_utilities/screens/stats_screen.dart';
+import 'package:easy_utilities/core/hex_color.dart';
 
 class LandingScreen extends StatefulWidget {
   @override
-  _LandingScreenState createState() => _LandingScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _LandingScreenState extends State<LandingScreen> {
+class _HomeScreenState extends State<LandingScreen> {
+  int _selectedIndex = 0;
+
+  PageController _pageController = PageController();
+
+  List<Widget> _screens = [
+    HomeScreen(),
+    BillsScreen(bill: null),
+    StatsScreen(),
+    AccountScreen(),
+  ];
+
+  void _onItemTapped(int selectIndex) {
+    _pageController.animateToPage(selectIndex,
+        duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Padding(padding: EdgeInsets.only(top: 50.0)),
-          Container(
-            child: Text(
-              'Easily Track Your Utility Spend.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.purple[10],
-                fontSize: 24.0,
-                fontFamily: 'Nunito',
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.0,
-              ),
+    return SafeArea(
+      child: Scaffold(
+        body: PageView(
+          controller: _pageController,
+          children: _screens,
+          onPageChanged: _onPageChanged,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          selectedLabelStyle: TextStyle(color: Colors.white),
+          selectedItemColor: Colors.white,
+          backgroundColor: HexColor.fromHex('#4A4040'),
+          onTap: _onItemTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-          ),
-          SizedBox(
-            height: 100.0,
-          ),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: [
-                    MaterialButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed('/bills',
-                            arguments: Constants.ELECTRICITY);
-                      },
-                      color: Colors.blue,
-                      textColor: Colors.deepOrange[50],
-                      child: Icon(
-                        Icons.power,
-                        size: 40,
-                      ),
-                      padding: EdgeInsets.all(16),
-                      shape: CircleBorder(),
-                    ),
-                    Padding(padding: EdgeInsets.only(top: 20.0)),
-                    Text('Add Electricity bill'),
-                  ],
-                ),
-                Column(
-                  children: [
-                    MaterialButton(
-                      onPressed: () {
-                        Navigator.of(context)
-                            .pushNamed('/bills', arguments: Constants.WATER);
-                      },
-                      color: Colors.blue,
-                      textColor: Colors.deepOrange[50],
-                      child: Icon(
-                        Icons.water_damage,
-                        size: 40,
-                      ),
-                      padding: EdgeInsets.all(16),
-                      shape: CircleBorder(),
-                    ),
-                    Padding(padding: EdgeInsets.only(top: 20.0)),
-                    Text('Add Water bill'),
-                  ],
-                ),
-              ]),
-          SizedBox(
-            height: 80,
-          ),
-          Text(
-            'R 1200.00',
-            style: TextStyle(
-              fontSize: 30.0,
-              letterSpacing: 2.0,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.money),
+              label: 'Add Utility',
             ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            'OVERALL SPEND',
-            style: TextStyle(fontSize: 15.0, letterSpacing: 1.0),
-          ),
-        ],
+            BottomNavigationBarItem(
+              icon: Icon(Icons.data_usage_outlined),
+              label: 'Statistics',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Account',
+            ),
+          ],
+        ),
       ),
     );
   }
