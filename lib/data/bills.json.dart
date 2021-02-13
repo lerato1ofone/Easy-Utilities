dc9f8f70fde9dac1f9bb52a654e792bd064887a2
Lerato Letsepe <letsepe100@gmail.com>
2021-02-13 16:05:37 +0200

    Added bills data, base

---
+++ lib/data/bills.json.dart
@@ -0,0 +1,60 @@
+import 'package:easy_utilities/data/bill_type.dart';
+
+var bills = [
+  {
+    "amount": 250.0,
+    "date": "2021–01–06T00:00:05.621389",
+    "type": bill_type.electricity,
+    "kwh": null,
+    "by": "Mashoto"
+  },
+  {
+    "amount": 100.0,
+    "date": "2021–01–10T00:00:05.621389",
+    "type": bill_type.electricity,
+    "kwh": null,
+    "by": "Lerato"
+  },
+  {
+    "amount": 50.0,
+    "date": "2021–01–18T00:00:05.621389",
+    "type": bill_type.water,
+    "kwh": null,
+    "by": "Lerato"
+  },
+  {
+    "amount": 100.0,
+    "date": "2021–01–25T00:00:05.621389",
+    "type": bill_type.electricity,
+    "kwh": null,
+    "by": "Lerato"
+  },
+  {
+    "amount": 100.0,
+    "date": "2021–01–27T00:00:05.621389",
+    "type": bill_type.water,
+    "kwh": null,
+    "by": "Mashoto"
+  },
+  {
+    "amount": 250.0,
+    "date": "2021–01–30T00:00:05.621389",
+    "type": bill_type.electricity,
+    "kwh": null,
+    "by": "Lerato"
+  },
+  {
+    "amount": 100.0,
+    "date": "2021–02–09T00:00:05.621389",
+    "type": bill_type.water,
+    "kwh": null,
+    "by": "Mashoto"
+  },
+  {
+    "amount": 300.0,
+    "date": "2021–02–13T00:00:05.621389",
+    "type": bill_type.electricity,
+    "kwh": null,
+    "by": "Mashoto"
+  }
+]
