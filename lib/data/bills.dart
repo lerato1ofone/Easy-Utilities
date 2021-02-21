dc8b185aeee469de1797be19defaae9947618dc5
Lerato Letsepe <letsepe100@gmail.com>
2021-02-21 18:17:35 +0200

    Updated bills json format, included packages.

--- lib/data/bills.dart
+++ lib/data/bills.dart
@@ -3,56 +3,56 @@ import 'package:easy_utilities/data/constants.dart';
 
 List<Bill> Bills = <Bill>[
   new Bill.fromJson({
-    'amount': 250.0,
-    'date': new DateTime(2021, 1, 06),
+    'amount': 250.00,
+    'date':   new DateTime(2021, 1, 06),
     'type': Constants.WATER,
     'kwh': null,
     'by': 'Mashoto'
   }),
   new Bill.fromJson({
-    'amount': 100.0,
+    'amount': 100.00,
     'date': new DateTime(2021, 1, 10),
     'type': Constants.ELECTRICITY,
     'kwh': null,
     'by': 'Lerato'
   }),
   new Bill.fromJson({
-    'amount': 50.0,
+    'amount': 50.00,
     'date': new DateTime(2021, 1, 18),
     'type': Constants.WATER,
     'kwh': null,
     'by': 'Lerato'
   }),
   new Bill.fromJson({
-    'amount': 100.0,
+    'amount': 100.00,
     'date': new DateTime(2021, 1, 25),
     'type': Constants.ELECTRICITY,
     'kwh': null,
     'by': 'Lerato'
   }),
   new Bill.fromJson({
-    'amount': 100.0,
+    'amount': 100.00,
     'date': new DateTime(2021, 1, 27),
     'type': Constants.WATER,
     'kwh': null,
     'by': 'Mashoto'
   }),
   new Bill.fromJson({
-    'amount': 250.0,
+    'amount': 250.00,
     'date': new DateTime(2021, 1, 30),
     'type': Constants.ELECTRICITY,
     'kwh': null,
     'by': 'Lerato'
   }),
   new Bill.fromJson({
-    'amount': 100.0,
+    'amount': 100.00,
     'date': new DateTime(2021, 2, 09),
     'type': Constants.WATER,
     'kwh': null,
     'by': 'Mashoto'
   }),
   new Bill.fromJson({
-    'amount': 300.0,
+    'amount': 300.00,
     'date': new DateTime(2021, 2, 13),
     'type': Constants.ELECTRICITY,
     'kwh': null,
