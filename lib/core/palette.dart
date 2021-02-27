6b52fcc18dcf56b71794a26f7e0ca07a577616e9
Lerato Letsepe <letsepe100@gmail.com>
2021-02-27 23:53:11 +0200

    Added heading and body styles, heading on login screen and input field for email address.

---
+++ lib/core/palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const TextStyle eHeading = TextStyle(
+  fontSize: 45.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+  fontFamily: 'Nunito'
+);
+
+const TextStyle eBodyText = TextStyle(
+    fontSize: 18.5,
+    color: Colors.white,
+    fontFamily: 'Nunito'
+);
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:easy_utilities/core/palette.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../widgets/widgets.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,8 +16,50 @@ class _LoginScreenState extends State<LoginScreen> {
         BackgroundImage(),
         Scaffold(
           backgroundColor: Colors.transparent,
-          body: Column(
-            
+          body: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  height: 150.0,
+                  child: Center(
+                    child: Text('Easy Utilities', style: eHeading),
+                  ),
+                ),
+                SizedBox(
+                  height: 100.0,
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(16.0)
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(vertical: 16.0),
+                            border: InputBorder.none,
+                            hintText: 'Email',
+                            hintStyle: eBodyText,
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal:20.0),
+                              child: Icon(
+                                FontAwesomeIcons.envelope, color: Colors.white,
+                                  size: 30.0,
+                              ),
+                            ),
+                          ),
+                          style: eBodyText,
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ],
