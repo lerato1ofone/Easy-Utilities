9ae417576de5241bd4fbd73cbd10f3feeb52574f
Lerato Letsepe <letsepe100@gmail.com>
2021-02-27 23:27:07 +0200

    Created background img widget

---
+++ lib/widgets/background_image.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class BackgroundImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (bounds) =>
+          LinearGradient(
+              colors: [Colors.black54, Colors.black12],
+              begin: Alignment.bottomCenter,
+              end: Alignment.center
+          ).createShader(bounds),
+      blendMode: BlendMode.darken,
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('./assets/images/login.jpg'),
+                fit: BoxFit.cover,
+                colorFilter:
+                ColorFilter.mode(Colors.black45, BlendMode.darken))),
+      ),
+    );
+  }
+}
