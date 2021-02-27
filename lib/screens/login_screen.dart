090cb3cee090d0b8afbf9bf8d12ca0969e039789
Lerato Letsepe <letsepe100@gmail.com>
2021-02-27 23:13:27 +0200

    Login background

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,20 +8,14 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-        body: Stack(children: <Widget>[
-          Container(
-            height: MediaQuery
-                .of(context)
-                .size
-                .height,
-            child: Image(
-              image: AssetImage('./assets/images/login.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ]),
-      );
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+         image: AssetImage('./assets/images/login.jpg'),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken)
+        )
+      ),
+    );
   }
 }
