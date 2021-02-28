61780f1a7ac89fbcf857465431affa47e5501261
Lerato Letsepe <letsepe100@gmail.com>
2021-02-28 15:26:42 +0200

    Added text and passowrd inputs in new files.

---
+++ lib/widgets/password_input.dart
@@ -0,0 +1,47 @@
+import 'package:easy_utilities/core/palette.dart';
+import 'package:flutter/material.dart';
+
+class PasswordInput extends StatelessWidget {
+  const PasswordInput(
+      {Key key,
+        @required this.icon,
+        @required this.hint,
+        @required this.inputAction})
+      : super(key: key);
+
+  final IconData icon;
+  final String hint;
+  final TextInputAction inputAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[600].withOpacity(0.5),
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(vertical: 16.0),
+            border: InputBorder.none,
+            hintText: hint,
+            hintStyle: eBodyText,
+            prefixIcon: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Icon(
+                icon,
+                color: Colors.white,
+                size: 30.0,
+              ),
+            ),
+          ),
+          style: eBodyText,
+          textInputAction: inputAction,
+          obscureText: true,
+        ),
+      ),
+    );
+  }
+}
