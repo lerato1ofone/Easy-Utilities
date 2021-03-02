2f66d4e7c10b9c4cf9da38faba83de596f52ac2c
lerato1ofone <letsepe100@gmail.com>
2021-03-02 23:58:45 +0200

    default function for register

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
-class AuthService{
-
-
-  // register with email and password
+class AuthService {
+  // register with email or phonenumber and password
+  Future registerWithEmailOrPhonenumberAndPassword(
+      String emailOrPhonenumber, String password) async {
+    try {} catch (e) {}
+  }
 }
