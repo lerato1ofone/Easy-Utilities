025905059fa488751fe81264025b18c24f1aab40
lerato1ofone <letsepe100@gmail.com>
2021-03-06 21:32:12 +0200

    Addec complete profile screen

---
+++ lib/screens/complete_profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompleteProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Complete your profile'),
+    );
+  }
+}
