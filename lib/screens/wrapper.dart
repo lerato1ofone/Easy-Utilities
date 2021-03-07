108dfde54ba62d183029ccdcad1112e58e704b54
lerato1ofone <letsepe100@gmail.com>
2021-03-07 11:56:57 +0200

    Used stream in wrapper widget :)

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,5 +1,6 @@
 import 'package:easy_utilities/models/user.dart';
 import 'package:easy_utilities/screens/landing_screen.dart';
+import 'package:easy_utilities/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,13 +9,25 @@ import 'authenticate/authenticate.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final user = Provider.of<User>(context);
 
-    if(user == null){
+    if (user == null) {
       return Authenticate();
     } else {
-      return LandingScreen();
+      return StreamBuilder<UserData>(
+          stream: DatabaseService(uid: user.uid).userData,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              UserData userData = snapshot.data;
+              return LandingScreen(
+                  isPostSignUp: !userData.profileUpdated,
+                  userName: userData.name);
+            } else {
+              return Container(
+                child: Text('Loading...'),
+              );
+            }
+          });
     }
   }
 }
