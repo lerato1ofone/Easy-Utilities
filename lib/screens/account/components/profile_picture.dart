c09ae47ab0a2db298b9f82b7b722c064e22cf3fc
lerato1ofone <letsepe100@gmail.com>
2021-03-08 00:11:41 +0200

    Added default-profile-img

--- lib/screens/account/components/profile_picture.dart
+++ lib/screens/account/components/profile_picture.dart
@@ -4,7 +4,10 @@ import 'package:flutter_svg/flutter_svg.dart';
 class ProfilePicture extends StatelessWidget {
   const ProfilePicture({
     Key key,
+    @required this.image,
   }) : super(key: key);
+
+  final String image;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +19,8 @@ class ProfilePicture extends StatelessWidget {
         fit: StackFit.expand,
         children: [
           CircleAvatar(
-            backgroundImage: AssetImage('./assets/images/profile-img.jpg'),
+            backgroundImage: AssetImage(image),
+            backgroundColor: Colors.white,
           ),
           Positioned(
             right: -10,
@@ -26,16 +30,17 @@ class ProfilePicture extends StatelessWidget {
               width: 46,
               // ignore: deprecated_member_use
               child: FlatButton(
-                  padding: EdgeInsets.all(10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    side: BorderSide(color: Colors.white),
-                  ),
-                  color: Color(0XFFF5F6F9),
-                  onPressed: () {},
-                  child: SvgPicture.asset(
-                    './assets/icons/camera-icon.svg',
-                  )),
+                padding: EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  side: BorderSide(color: Colors.white),
+                ),
+                color: Color(0XFFF5F6F9),
+                onPressed: () {},
+                child: SvgPicture.asset(
+                  './assets/icons/camera-icon.svg',
+                ),
+              ),
             ),
           )
         ],
