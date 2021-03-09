6211c536b750439669037af15c8a59314d34eb70
lerato1ofone <letsepe100@gmail.com>
2021-03-10 00:37:50 +0200

    Added bills collection and get bill documents

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,17 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easy_utilities/data/bill_type.dart';
+import 'package:easy_utilities/models/bill.dart';
 import 'package:easy_utilities/models/user.dart';
 
 class DatabaseService {
   final String uid;
   DatabaseService({this.uid});
 
-  // collection reference
-  final CollectionReference billsCollection =
+  // Accounts.
+
+  // collection reference accounts
+  final CollectionReference accountsCollection =
       Firestore.instance.collection('accounts');
 
   Future updateUserData(String name, String emailOrPhoneNumber, String password,
       String profilePhotoUrl, bool profileUpdated) async {
-    return await billsCollection.document(uid).setData({
+    return await accountsCollection.document(uid).setData({
       'name': name,
       'emailOrPhoneNumber': emailOrPhoneNumber,
       'password': password,
@@ -21,7 +25,7 @@ class DatabaseService {
   }
 
   Future<bool> getUserProfileUpdateStatus() async {
-    await billsCollection.document(uid).get().then((doc) {
+    await accountsCollection.document(uid).get().then((doc) {
       return doc.data['profileUpdated'];
     });
     return true;
@@ -39,6 +43,68 @@ class DatabaseService {
 
   // Get user document
   Stream<UserData> get userData {
-    return billsCollection.document(uid).snapshots().map(_userDataFromSnapshot);
+    return accountsCollection
+        .document(uid)
+        .snapshots()
+        .map(_userDataFromSnapshot);
+  }
+
+  // Bills.
+
+  // collection reference bills
+  final CollectionReference billsCollection =
+      Firestore.instance.collection('bills');
+
+  Future updateBillData(String uid, double amount, DateTime date, BillType type,
+      double kwh, double litres, String userId) async {
+    return await billsCollection.document(uid).setData({
+      'uid': uid,
+      'amount': amount,
+      'date': date,
+      'type': type,
+      'kwh': kwh,
+      'litres': litres,
+      'userId': userId
+    });
+  }
+
+  // BillData snapshot
+  BillData _billDataFromSnapshot(DocumentSnapshot snapshot) {
+    return BillData(
+        uid: uid,
+        amount: double.parse(snapshot.data['amount']),
+        date: DateTime.fromMicrosecondsSinceEpoch(
+            snapshot.data['date'].microsecondsSinceEpoch),
+        type: snapshot.data['type'],
+        kwh: double.parse(snapshot.data['kwh']),
+        litres: double.parse(snapshot.data['litres']),
+        userId: snapshot.data['userId']);
+  }
+
+  // Get bill document
+  Stream<BillData> get billData {
+    return accountsCollection
+        .document(uid)
+        .snapshots()
+        .map(_billDataFromSnapshot);
+  }
+
+  Future<List<BillData>> getBillsData(bool isLatest) async {
+    // Get docs from collection reference
+    QuerySnapshot querySnapshot = await billsCollection.getDocuments();
+
+    // ignore: deprecated_member_use
+    List<BillData> bills = new List<BillData>();
+
+    final allData = querySnapshot.documents
+        .map((doc) => bills.add(_billDataFromSnapshot(doc)));
+
+    print(allData);
+
+    if (isLatest) {
+      return bills;
+    } else {
+      return bills;
+    }
   }
 }
