cd9322554874440a9dd0707e9a51d783eef59ffd
lerato1ofone <letsepe100@gmail.com>
2021-03-14 18:37:55 +0200

    Touch up bill screen

--- lib/screens/bill/add_bill_screen.dart
+++ lib/screens/bill/add_bill_screen.dart
@@ -1,6 +1,8 @@
 import 'package:easy_utilities/core/hex_color.dart';
 import 'package:easy_utilities/core/palette.dart';
+import 'package:easy_utilities/data/bill_type.dart';
 import 'package:easy_utilities/models/user.dart';
+import 'package:easy_utilities/widgets/option_instruction.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -16,8 +18,11 @@ class AddBillScreen extends StatefulWidget {
 }
 
 class _AddBillScreenState extends State<AddBillScreen> {
-  int _groupValue = -1;
   DateTime selectedDate = DateTime.now();
+  bool dateSelected = false;
+  String type;
+  double amount = 0.00;
+  int groupValue = -1;
 
   Future<Null> _selectDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
@@ -28,7 +33,14 @@ class _AddBillScreenState extends State<AddBillScreen> {
     if (picked != null && picked != selectedDate)
       setState(() {
         selectedDate = picked;
+        dateSelected = true;
       });
+  }
+
+  void onChanged(String value) {
+    type = value;
+    print(type);
+    print(groupValue);
   }
 
   @override
@@ -71,14 +83,34 @@ class _AddBillScreenState extends State<AddBillScreen> {
               ),
             ),
             child: Padding(
-              padding: const EdgeInsets.only(left: 35, top: 8),
-              child: Text(
-                'R     ______',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.normal,
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextFormField(
+                validator: (value) => value.isEmpty ? 'Enter an amount' : null,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(vertical: 16.0),
+                  border: InputBorder.none,
+                  hintText: 'Amount',
+                  hintStyle: TextStyle(
+                      fontSize: 18.5,
+                      color: HexColor.fromHex('#4D4D4D'),
+                      fontFamily: 'Roboto',
+                      letterSpacing: 0.2,
+                      fontWeight: FontWeight.w500),
+                  fillColor: Colors.black,
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: SvgPicture.asset(
+                      "./assets/icons/south-african-rand.svg",
+                      width: 40,
+                      height: 40,
+                      color: HexColor.fromHex('#4D4D4D'),
+                    ),
+                  ),
                 ),
+                style: eBodyText1,
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.done,
+                onChanged: (val) => onAmountChanged(val),
               ),
             ),
           ),
@@ -89,32 +121,9 @@ class _AddBillScreenState extends State<AddBillScreen> {
             padding: EdgeInsets.symmetric(horizontal: 50),
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Please enter a date',
-                      style: eQuestionText,
-                    ),
-                    Container(
-                      decoration: new BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 1.5,
-                        ),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(8.0),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          '1 REQUIRED',
-                          style: eRequiredText,
-                        ),
-                      ),
-                    ),
-                  ],
+                OptionInstruction(
+                  text: 'Please enter a date',
+                  requiredNo: 1,
                 ),
                 SizedBox(
                   height: 20,
@@ -133,37 +142,78 @@ class _AddBillScreenState extends State<AddBillScreen> {
                     child: Column(
                       children: [
                         Expanded(
-                          child: RadioListTile(
-                            value: 0,
-                            groupValue: _groupValue,
-                            title: Text("Use today’s date"),
-                            onChanged: (newValue) =>
-                                setState(() => _groupValue = newValue),
-                            activeColor: Colors.blue,
-                            selected: false,
-                            toggleable: true,
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 13.0),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    _selectDate(context);
+                                  },
+                                  child: CircleAvatar(
+                                    radius: 30,
+                                    backgroundColor: Colors.blueGrey[200],
+                                    child: SvgPicture.asset(
+                                      "./assets/icons/calendar-icon.svg",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 30,
+                              ),
+                              dateSelected == false
+                                  ? Text("Tap icon to add date")
+                                  : Text(
+                                      "${selectedDate.toLocal()}".split(' ')[0],
+                                      style: TextStyle(
+                                          color: HexColor.fromHex('#7C7C7C'),
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.w500,
+                                          fontFamily: 'Roboto'),
+                                    ),
+                            ],
                           ),
                         ),
-                        Expanded(
-                            child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 13.0),
-                              child: IconButton(
-                                icon: SvgPicture.asset(
-                                  "./assets/icons/calendar-icon.svg",
-                                  height: 22,
-                                  width: 22,
-                                ),
-                                onPressed: () => _selectDate(context),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text('Tap to add a date')
-                          ],
-                        )),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                OptionInstruction(
+                  text: 'Choose the utility type',
+                  requiredNo: 1,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 130,
+                  width: 280,
+                  decoration: new BoxDecoration(
+                    color: HexColor.fromHex('#F8F8F8'),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        RadioListTile(
+                          value: type,
+                          groupValue: groupValue,
+                          onChanged: (value) => onChanged(value),
+                          title: Text('Electricity'),
+                        ),
+                        RadioListTile(
+                          value: type,
+                          groupValue: groupValue,
+                          onChanged: (value) => onChanged(value),
+                          title: Text('Water'),
+                        ),
                       ],
                     ),
                   ),
@@ -174,5 +224,11 @@ class _AddBillScreenState extends State<AddBillScreen> {
         ],
       ),
     );
+  }
+
+  void onAmountChanged(value) {
+    setState(() {
+      amount = value;
+    });
   }
 }
