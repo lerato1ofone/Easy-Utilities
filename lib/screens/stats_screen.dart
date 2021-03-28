4c38d166ab81344cc7a9978c58a0c822453356b0
lerato1ofone <letsepe100@gmail.com>
2021-03-28 21:33:53 +0200

    Stats screen base, header and search bar

--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -1,4 +1,6 @@
+import 'package:easy_utilities/core/palette.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class StatsScreen extends StatefulWidget {
   @override
@@ -9,11 +11,49 @@ class _StatsScreenState extends State<StatsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Statistics'),
-      ),
-      body: Center(
-        child: Text('Stats Screen'),
+      backgroundColor: Colors.grey.withOpacity(0.05),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    var size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.01),
+                    spreadRadius: 10,
+                    blurRadius: 3),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 60, left: 20, right: 20, bottom: 25),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Stats",
+                        style: eTitleText,
+                      ),
+                      SvgPicture.asset(
+                        './assets/icons/search-icon.svg',
+                        width: 30,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
