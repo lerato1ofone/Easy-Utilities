8747cac9c6c84c70e275b47d36300245b73cc457
lerato1ofone <letsepe100@gmail.com>
2021-03-28 22:16:06 +0200

    Added const list for days and months. and removed empty line from pubspec.yaml

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -2,3 +2,23 @@ class Constants {
   static const String WATER = "Water";
   static const String ELECTRICITY = "Electricity";
 }
+
+List days = [
+  {"label": "Sun", "day": "28"},
+  {"label": "Mon", "day": "29"},
+  {"label": "Tue", "day": "30"},
+  {"label": "Wed", "day": "1"},
+  {"label": "Thu", "day": "2"},
+  {"label": "Fri", "day": "3"},
+  {"label": "Sat", "day": "4"}
+];
+
+List months = [
+  {"label": "2021", "day": "Jan"},
+  {"label": "2021", "day": "Feb"},
+  {"label": "2021", "day": "Mar"},
+  {"label": "2021", "day": "Apr"},
+  {"label": "2021", "day": "May"},
+  {"label": "2021", "day": "Jun"},
+  {"label": "2021", "day": "Jul"},
+];
