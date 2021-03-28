1f58daec7032d999796e31b2e0e9258c868d0b68
lerato1ofone <letsepe100@gmail.com>
2021-03-28 22:16:24 +0200

    Added days and month scroller on stats_screen

--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -1,5 +1,8 @@
+import 'package:easy_utilities/core/hex_color.dart';
 import 'package:easy_utilities/core/palette.dart';
+import 'package:easy_utilities/data/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class StatsScreen extends StatefulWidget {
@@ -8,6 +11,7 @@ class StatsScreen extends StatefulWidget {
 }
 
 class _StatsScreenState extends State<StatsScreen> {
+  int activeMonth = 4;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,7 +52,71 @@ class _StatsScreenState extends State<StatsScreen> {
                         width: 30,
                       ),
                     ],
-                  )
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: List<Widget>.generate(
+                        days.length,
+                        (index) {
+                          print(index);
+                          return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  activeMonth = index;
+                                });
+                              },
+                              child: Container(
+                                width: (size.width - 40) / 7,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      days[index]['label'],
+                                      style: TextStyle(fontSize: 10),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: activeMonth == index
+                                            ? Colors.red
+                                            : Colors.transparent,
+                                        borderRadius: BorderRadius.circular(5),
+                                        border: Border.all(
+                                          color: activeMonth == index
+                                              ? HexColor.fromHex('#000')
+                                              : Colors.black.withOpacity(0.1),
+                                        ),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 7,
+                                            bottom: 7,
+                                            right: 12,
+                                            left: 12),
+                                        child: Text(
+                                          months[index]['day'],
+                                          style: TextStyle(
+                                            fontSize: 10,
+                                            color: activeMonth == index
+                                                ? Colors.white
+                                                : Colors.black,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ));
+                        },
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
