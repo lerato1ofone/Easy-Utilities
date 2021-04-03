268bc92a9e74064e0870b1f41c6758f035c6ea1e
lerato1ofone <letsepe100@gmail.com>
2021-04-03 23:59:17 +0200

    Added filter chip widget

--- lib/screens/stats/filter_screen.dart
+++ lib/screens/stats/filter_screen.dart
@@ -1,4 +1,6 @@
 import 'package:easy_utilities/core/hex_color.dart';
+import 'package:easy_utilities/core/palette.dart';
+import 'package:easy_utilities/widgets/filter_chip_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -7,20 +9,37 @@ class StatsFilters extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: HexColor.fromHex('#E5DFFE'),
-      body: Container(
-        child: Align(
-          alignment: Alignment.topRight,
-          child: IconButton(
-            icon: SvgPicture.asset(
-              "./assets/icons/cancel-icon.svg",
-              color: HexColor.fromHex('#4D4D4D'),
+      body: Column(
+        children: [
+          Container(
+            child: Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: SvgPicture.asset(
+                  "./assets/icons/cancel-icon.svg",
+                  color: HexColor.fromHex('#4D4D4D'),
+                ),
+                iconSize: 100,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
             ),
-            iconSize: 100,
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
           ),
-        ),
+          Container(
+            child: Text('Stats FIlters', style: eTitleText),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Wrap(
+            spacing: 5.0,
+            runSpacing: 3.0,
+            children: <Widget>[
+              FilterChipWidget(chipName: 'Filters'),
+            ],
+          )
+        ],
       ),
     );
   }
---
+++ lib/widgets/filter_chip_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FilterChipWidget extends StatefulWidget {
+  FilterChipWidget({
+    Key key,
+    this.chipName,
+  }) : super(key: key);
+
+  final String chipName;
+  @override
+  _FilterChipWidgetState createState() => _FilterChipWidgetState();
+}
+
+class _FilterChipWidgetState extends State<FilterChipWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
