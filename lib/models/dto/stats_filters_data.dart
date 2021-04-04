93e4357c0577568003b7dc7bfdc5ccc91f34de5d
lerato1ofone <letsepe100@gmail.com>
2021-04-04 04:17:49 +0200

    Updated stats filters data to add more filter properties

--- lib/models/dto/stats_filters_data.dart
+++ lib/models/dto/stats_filters_data.dart
@@ -3,6 +3,11 @@ import 'package:easy_utilities/data/bill_type.dart';
 class StatsFiltersData {
   final List<String> names;
   final List<BillType> billTypes;
+  final double minPrice;
+  final double maxPrice;
+  final DateTime endDate;
+  final DateTime startDate;
 
-  StatsFiltersData(this.names, this.billTypes);
+  StatsFiltersData(this.names, this.billTypes, this.minPrice, this.maxPrice,
+      this.startDate, this.endDate);
 }
